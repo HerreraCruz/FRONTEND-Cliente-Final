aa011463451aa7c0fa01956b9bb0be6249d8b8a2
HerreraCruz <andre.herrera@unah.hn>
2022-11-20 22:04:25 -0600

    más avance

--- assets/js/controlador.js
+++ assets/js/controlador.js
@@ -4,26 +4,27 @@ var usuarios = [
         nombre: "Goku",
         imagenPerfil: "assets/img/profile-pics/goku.jpg",
         correo: 'goku@unah.hn',
+        contraseña:'madrid',
         direccion: 'boulevard suyapa',
         numero: 99887766,
         pedidos: [
             {
                 category: 1,
-                correctas: 4,
-                incorrectas: 1,
-                aprobadas: false,
+                socios: 4,
+                productos: 1,
+                unidades: false,
             },
             {
                 category: 2,
-                correctas: 5,
-                incorrectas: 0,
-                aprobadas: true,
+                socios: 5,
+                productos: 0,
+                unidades: true,
             },
             {
                 category: 3,
-                correctas: 5,
-                incorrectas: 0,
-                aprobadas: true,
+                socios: 5,
+                productos: 0,
+                unidades: true,
             }
         ]
     },
@@ -32,26 +33,27 @@ var usuarios = [
         nombre: "videl",
         imagenPerfil: "assets/img/profile-pics/videl.jpg",
         correo: 'videl@unah.hn',
+        contraseña:'motagua',
         direccion: 'boulevard suyapa',
         numero: 99887766,
         pedidos: [
             {
                 category: 1,
-                correctas: 4,
-                incorrectas: 1,
-                aprobadas: false,
+                socios: 4,
+                productos: 1,
+                unidades: false,
             },
             {
                 category: 2,
-                correctas: 5,
-                incorrectas: 0,
-                aprobadas: true,
+                socios: 5,
+                productos: 0,
+                unidades: true,
             },
             {
                 category: 3,
-                correctas: 5,
-                incorrectas: 0,
-                aprobadas: true,
+                socios: 5,
+                productos: 0,
+                unidades: true,
             }
         ]
     },
@@ -60,26 +62,27 @@ var usuarios = [
         nombre: "freezer",
         imagenPerfil: "assets/img/profile-pics/freezer.jpg",
         correo: 'freezer@unah.hn',
+        contraseña:'olimpia',
         direccion: 'boulevard suyapa',
         numero: 99887766,
         pedidos: [
             {
                 category: 1,
-                correctas: 4,
-                incorrectas: 1,
-                aprobadas: false,
+                socios: 4,
+                productos: 1,
+                unidades: false,
             },
             {
                 category: 2,
-                correctas: 5,
-                incorrectas: 0,
-                aprobadas: true,
+                socios: 5,
+                productos: 0,
+                unidades: true,
             },
             {
                 category: 3,
-                correctas: 5,
-                incorrectas: 0,
-                aprobadas: true,
+                socios: 5,
+                productos: 0,
+                unidades: true,
             }
         ]
     },
@@ -88,26 +91,27 @@ var usuarios = [
         nombre: "gohan",
         imagenPerfil: "assets/img/profile-pics/gohan.jpg",
         correo: 'gohan@unah.hn',
+        contraseña:'barcelona',
         direccion: 'boulevard suyapa',
         numero: 99887766,
         pedidos: [
             {
                 category: 1,
-                correctas: 4,
-                incorrectas: 1,
-                aprobadas: false,
+                socios: 4,
+                productos: 1,
+                unidades: false,
             },
             {
                 category: 2,
-                correctas: 5,
-                incorrectas: 0,
-                aprobadas: true,
+                socios: 5,
+                productos: 0,
+                unidades: true,
             },
             {
                 category: 3,
-                correctas: 5,
-                incorrectas: 0,
-                aprobadas: true,
+                socios: 5,
+                productos: 0,
+                unidades: true,
             }
         ]
     }
@@ -935,11 +939,23 @@ var categorias = [
     }
 ];
 
+var pedidosPendientes =[
+    {
+        id: 0,
+        category: 0,
+        socios: 0,
+        productos: 0,
+        unidades: 0,
+    }
+]
 
 
 //console.log('Usuarios', usuarios);
 
 console.log('ver',categorias[0].socios);
+console.log('ver',categorias.length);
+console.log('ver',usuarios.length);
+var unit;
 
 
 var localstorage = window.localStorage;
@@ -953,6 +969,8 @@ if(localStorage.getItem('categorias')== null) {
 }
 console.log('a', JSON.parse(localstorage.getItem('categorias'))[0].socios);
 console.log('b', JSON.parse(localstorage.getItem('categorias'))[0].socios[0].id);
+
+console.log('c', JSON.parse(localstorage.getItem('usuarios')).length);
 
 function login() {
     if (document.getElementsByClassName('origen0')[0].id!='titulo') {
@@ -1075,6 +1093,7 @@ function generarProductos(a,b,c) {
 }
 
 function compraProducto(a,b,c,d) {
+    unit=1;
     document.getElementById('categoria').innerHTML=null;
     document.getElementById('titulo2').innerHTML = 
         `<div><i class="fa-solid fa-angle-left" onclick="generarProductos(${a},${b},${c})"></i></div>
@@ -1082,35 +1101,45 @@ function compraProducto(a,b,c,d) {
          <div><i class="fa-solid fa-bars" onclick="menu()" data-bs-toggle="modal" data-bs-target="#exampleModal"></i></div>`;
     document.getElementById('categoria').innerHTML =
         `<div id="compra"><img src="assets/img/productos/bigos/bigos.jpg" style="border-radius: 15%; width:350px;">
-        <br><br><br>
-        <button class="btn col-6" id="botonSelect">
-                <img src="${JSON.parse(localStorage.getItem('categorias'))[b-1].socios[c-1].productos[d-1].imagen}" style="border-radius: 15%; width:150px; height:150px;">
-                <div id="rubro">${JSON.parse(localStorage.getItem('categorias'))[b-1].socios[c-1].productos[d-1].nombre}
-                </div>      
-        </button>
-        <br><div id="rubro" style="font-size:2rem;">L ${JSON.parse(localStorage.getItem('categorias'))[b-1].socios[c-1].productos[d-1].precio}.00</div><br>
-        
-        <div style="font-size:2rem; width:100%; display: flex; flex-direction: row; flex-wrap: nowrap; justify-content: space-evenly; align-items: center; align-content: center;" id="rubro"><div><i class="fa-solid fa-circle-plus"></i></div><div>cantidad</div><div><i class="fa-solid fa-circle-minus"></i></div></div>
-        <div id="titulo3">AGREGAR A ORDEN</div>
+            <br><br><br>
+            <button class="btn col-6" id="botonSelect">
+                    <img src="${JSON.parse(localStorage.getItem('categorias'))[b-1].socios[c-1].productos[d-1].imagen}" style="border-radius: 15%; width:150px; height:150px;">
+                    <div id="rubro">${JSON.parse(localStorage.getItem('categorias'))[b-1].socios[c-1].productos[d-1].nombre}</div>      
+            </button>
+            <br><div id="rubro" style="font-size:2rem;">L ${JSON.parse(localStorage.getItem('categorias'))[b-1].socios[c-1].productos[d-1].precio}.00</div><br>
+            
+            <div style="font-size:2rem; width:100%; display: flex; flex-direction: row; flex-wrap: nowrap; justify-content: space-evenly; align-items: center; align-content: center;" id="rubro">
+                <div onclick="cantidad(0)"><i class="fa-solid fa-circle-minus"></i></div>
+                <div id="num">${unit}</div>
+                <div onclick="cantidad(1)"><i class="fa-solid fa-circle-plus"></i></div>
+                
+            </div>
+            <div id="titulo3">AGREGAR A ORDEN</div>
         </div>`;
 }
 
 function registrarse() {
     
-    document.getElementById('titulo').innerHTML= `<div>Registrarse</div>`
+    document.getElementsByClassName('origen0')[0].id='titulo2'
+        document.getElementById('titulo2').innerHTML = 
+            `<div><i class="fa-solid fa-angle-left" onclick="login()"></i></div>
+            <div>aunClick-REGISTRO</div>
+            <div><i class="fa-solid fa-bars" onclick="menu()" data-bs-toggle="modal" data-bs-target="#exampleModal"></i></div>`
     document.getElementById('facetas').innerHTML =
         `<div class="container">
             <div id="registro">
             
-                    <input class="form-control" type="number" id="" placeholder="Nombre de Usuario" min="0" max="10">
-                    <input class="form-control" type="text" id="" placeholder="Correo Electrónico">
-                    <input class="form-control" type="text" id="" placeholder="Contraseña">
+                    <input class="form-control" type="text" id="name" placeholder="Nombre de Usuario">
+                    <input class="form-control" type="number" id="image" placeholder="Imagen perfil" min="1" max="2">
+                    <input class="form-control" type="text" id="email" placeholder="Correo Electrónico">
+                    <input class="form-control" type="password" id="password" placeholder="Contraseña">
+                    <input class="form-control" type="text" id="address" placeholder="Dirección">
+                    <input class="form-control" type="number" id="cell" placeholder="Número Celular" min="8" max="8">                   
 
-        
             </div>
          </div>
         <div id="login-signup">
-            <button id="login" onclick="generarUsuarios()">
+            <button id="login" onclick="guardar()">
                 SIGN UP
             </button>
             <hr>
@@ -1122,12 +1151,38 @@ function registrarse() {
 function guardar() {
     const user =  {
                 
-        id: localstorage.getItem('usuarios').length + 1,
-        nombre: document.getElementById('').value,
-        imagenPerfil: `assets/img/profile-pics/${document.getElementById('').value}`,
-        correo: document.getElementById('').value,
-        direccion: document.getElementById('').value,
-        numero: document.getElementById('').value,
+        id: JSON.parse(localstorage.getItem('usuarios')).length + 1,
+        nombre: document.getElementById('name').value,
+        imagenPerfil: `assets/img/profile-pics/${document.getElementById('image').value}.jpg`,
+        correo: document.getElementById('email').value,
+        contraseña:document.getElementById('password').value,
+        direccion: document.getElementById('address').value,
+        numero: document.getElementById('cell').value,
         pedidos: [],
-    }
+    };
+
+    console.log(user);
+    usuarios.push(user);
+    localStorage.setItem('usuarios', JSON.stringify(usuarios));
+    console.log(usuarios);
+
+    generarUsuarios();
+}
+
+
+
+function cantidad(valor) {
+    if (valor==1) {
+        unit++
+    } else if(unit<2) {}
+    else {
+        unit--
+    };
+    console.log(unit); 
+    document.getElementById('num').innerHTML = `${unit}`;
+}
+
+function ordenPendiente(params) {
+      
+    
 }
